67fd73f7eb31bdc9791b8cc624f1961613b253de
solc1us <cyan.magina@gmail.com>
2025-11-28 19:45:40 +0700

    first commit

---
+++ src/main/java/com/example/zej/ZejApplication.java
@@ -0,0 +1,13 @@
+package com.example.zej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZejApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zej/ZejApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZejApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
